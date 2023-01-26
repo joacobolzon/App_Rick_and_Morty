2ade2ef48ee0a37e40ed94bcb00bb44e6a3b700c
joacobolzon <joaco.bolzon3@gmail.com>
2023-01-26 01:57:36 -0300

    Forms terminada

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
+import { useState , useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Route , Routes } from 'react-router-dom'
 import './App.css'
 import Cards from './components/Cards/Cards.jsx'  
 import Nav from './components/Nav/Nav.jsx'
+
 import About from './components/About/About.jsx'
 import Detail from './components/Detail/Detail.jsx'
 import Error from './components/Error/Error.jsx'
-import { useState } from 'react'
-import { Route,Routes } from 'react-router-dom'
+import Form from './components/Form/Form.jsx'
 
 function App () {
-
-  const [characters,setCharacters] = useState([])
-
- const onSearch = (character) => {
-  fetch(`https://rickandmortyapi.com/api/character/${character}`)
-      .then((response) => response.json())
-      .then((data) => {
-         if (data.name) {
-            setCharacters((oldChars) => [...oldChars, data]);
-         } else {
-            window.alert('No hay personajes con ese ID');
-         }
-      });
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  const username = 'joaco@gmail.com';
+  const password = 'joaco123';
+  
+  const [access,setAccess] = useState(false);
+  
+  const [characters,setCharacters] = useState([]);
+  
+  function login(userData) {
+    if (userData.password === password && userData.username === username) {
+       setAccess(true);
+       navigate('/home');
+    }
  }
-
-const onClose = (id) => {
-  setCharacters(characters.filter(char => char.id !== id))
-}
-
-  return (
-    <div>
-    <Nav onSearch = {onSearch}/>
-    <Routes className='App' style={{ padding: '25px' }}>
-      <Route path='/home' element={<Cards characters={characters} onClose = {onClose}/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/detail/:detailId' element={<Detail/>}/>
-      <Route path='*' element={<Error/>}></Route>
-    </Routes>
-    </div>
+ 
+ const onSearch = (character) => {
+   fetch(`https://rickandmortyapi.com/api/character/${character}`)
+   .then((response) => response.json())
+   .then((data) => {
+     if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+          } else {
+            window.alert('No hay personajes con ese ID');
+          }
+        });
+      }
+      
+      const onClose = (id) => {
+        setCharacters(characters.filter(char => char.id !== id))
+      }
+      
+      useEffect(() => {
+       !access && navigate('/');
+     }, [access]);
+      
+      return (
+        <div>
+          {location.pathname !== '/' && <Nav onSearch = {onSearch}/>}
+            <Routes className='App' style={{ padding: '25px' }}>
+              <Route exact path='/' element={<Form login={login}/>}></Route>
+              <Route path='/home' element={<Cards characters={characters} onClose = {onClose}/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/detail/:detailId' element={<Detail/>}/>
+              <Route path='*' element={<Error/>}></Route>
+            </Routes>
+        </div>
   )
 }
 
